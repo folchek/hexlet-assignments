a4a05f8a02292b744a2282783e06a65a3b2081c4
@hexlet/cli <support@hexlet.io>
2024-12-19 23:09:10 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -9,6 +9,9 @@ import java.util.stream.Collectors;
 // BEGIN
 public class App {
    public static List<String> buildApartmentsList(List<Home> apartments, int n) {
+       if (n > apartments.size()) {
+           return new ArrayList<String>();
+       }
        List<Home> tempList = new ArrayList<>(apartments);
        tempList.sort(Comparator.comparing(Home::getArea));
 
@@ -19,16 +22,5 @@ public class App {
        }
        return sortedList;
    }
-
-    public static void main(String[] args) {
-        List<Home> apartments = new ArrayList<>(List.of(
-                new Flat(41, 3, 10),
-                new Cottage(125.5, 2),
-                new Flat(80, 10, 2),
-                new Cottage(150, 3)
-        ));
-        List<String> result = App.buildApartmentsList(apartments, 3);
-        System.out.println(result);
-    }
 }
 // END
